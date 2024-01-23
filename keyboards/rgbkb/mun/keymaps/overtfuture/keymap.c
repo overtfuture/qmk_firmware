660fc2de7f09875dd0eeb01cdda175531fcf267d
Nathan Grey <nate@grey.bz>
2024-01-23 12:02:18 -0500

    Updates keymap for the mun keyboard

--- keyboards/rgbkb/mun/keymaps/overtfuture/keymap.c
+++ keyboards/rgbkb/mun/keymaps/overtfuture/keymap.c
@@ -9,6 +9,9 @@ enum keymap_layers {
 enum keymap_keycodes {
     // Disables touch processing
     TCH_TOG = SAFE_RANGE,
+    MENU_BTN,
+    MENU_UP,
+    MENU_DN
 };
 
 // Default Layers
@@ -16,6 +19,9 @@ enum keymap_keycodes {
 
 // Momentary Layers
 #define FN       MO(_FN)
+
+#define FN_CAPS  LT(_FN, KC_CAPS)
+#define FN_ESC   LT(_FN, KC_ESC)
 
 /* This keyboard is enabled with an RGB Menu Control system.
 This functionality is enabled, but still requires a little configuration based on your exact setup.
@@ -31,27 +37,53 @@ Depending on where you add an encoder to the right half will determin in the def
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,   KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LCBR,   KC_RCBR,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,   KC_RBRC,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN,   KC_RPRN,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        FN,      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  FN,        FN,        KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_RCTL, FN,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,       _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,       _______,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,       _______,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,       _______,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        FN,      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  FN,            FN,        KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_RCTL, FN,
 
-        _______, _______, _______, _______,                                                           _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______
+        KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV,                                             RGB_HUI, RGB_HUD, RGB_RMOD,RGB_TOG, RGB_MOD
     ),
 
     [_FN] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   RGB_TOG,   RGB_TOG,  KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_DEL,
-        _______, _______, _______, KC_LBRC, KC_RBRC, _______, RGB_RMOD,  RGB_MOD, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, KC_LPRN, KC_RPRN, _______, RGB_VAD,   RGB_VAI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
-        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-        QK_BOOT, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, QK_BOOT,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,       _______,  KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_DEL,
+        _______, KC_MINS, KC_EQL , KC_LPRN, KC_RPRN, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_GRV , _______,       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
+        _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+        QK_BOOT, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, QK_BOOT,
 
-        _______, _______, _______, _______,                                                       _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______
+        KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV,                                             RGB_HUI, RGB_HUD, RGB_RMOD,RGB_TOG, RGB_MOD
     ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode)
+    {
+        case MENU_BTN:
+            if (record->event.pressed) {
+                rgb_menu_selection();
+            }
+            return false;
+        case MENU_UP:
+            if (record->event.pressed) {
+                rgb_menu_action(true);
+            }
+            return false;
+        case MENU_DN:
+            if (record->event.pressed) {
+                rgb_menu_action(false);
+            }
+            return false;
+        case TCH_TOG:
+            if (record->event.pressed) {
+                touch_encoder_toggle();
+            }
+            return false;  // Skip all further processing of this key
+        default:
+            return true;
+    }
+}
 
 static void render_layer(void) {
     // Host Keyboard Layer Status
@@ -105,3 +137,10 @@ bool oled_task_user(void) {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_270;
 }
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =       { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [1] =       { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
+};
+#endif
